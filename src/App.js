c15aa800b2ba1737d1fb892fea5cb6b2c0f0f572
vivek <vivekkumarvky13@gmail.com>
2021-12-23 18:46:55 +0530

    react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+ 
+  const arr = ["Android", "Blackberry", "iPhone", "Windows Phone"]
+  const manu = ["Samsung", "HTC","Micromax", "Apple"]
+  return(
+     <div className="App">
+
+       <h1>Mobile Operating System</h1>
+       {arr.map((e) =>(
+        <ul>
+          <li>{e}</li>
+        </ul>
+      ))}
+      <h1>Mobile Manufacturers</h1>
+       {manu.map((e) =>(
+        <ul>
+          <li>{e}</li>
+        </ul>
+      ))}
+  </div>
   );
 }
 
